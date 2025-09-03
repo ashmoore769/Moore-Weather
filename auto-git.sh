e8a813c99731fbf11fd60e82d626c70496093475
Ash Moore <ashmoore01@hotmail.com>
2025-09-03 22:47:09 +1000

    update from 2025-09-03 22:47:09

--- auto-git.sh
+++ auto-git.sh
@@ -1,3 +1,19 @@
+
+
+
+#!/bin/zsh
+# Simple auto-commit & push script
+# Usage: ./auto-git.sh "your commit message"
+
+if [ -z "$1" ]; then
+  msg="update from $(date '+%Y-%m-%d %H:%M:%S')"
+else
+  msg="$1"
+fi
+
+git add -A
+git commit -m "$msg"
+git push origin main
 #!/bin/bash
 
 fswatch -o public/index.html public/daily.html | while read num
